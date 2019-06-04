90f18febbd1d1a2f98f199271aff32d4b0a7f2af
anran <anrancai1020@gmail.com>
2019-06-04 18:28:05 +0200

    variable importance curve and prediction function  Please enter the commit message for your changes. Lines starting

--- random egg.R
+++ random egg.R
@@ -4,18 +4,25 @@ View(base_diabete_ml_sauf_sexe)
 
 library(randomForestSRC)
 diabetes = read.csv(file="base_diabete_ml_sauf_sexe.csv", sep = ';')
-v.obj <- rfsrc(Surv(suivi_fin, diabete) ~ ., data = diabetes, split.depth = c(FALSE, "all.trees", "by.tree"),
-               ntree = 100, block.size = 1, var.used="by.tree",splitrule="logrank")
-vs.v <- var.select(object = v.obj,method="vh")
+
+v.obj <- rfsrc(Surv(suivi_fin, diabete) ~ ., data = diabetes,bootstrap=c("by.root"),split.depth = "by.tree", ntree = 100, block.size = 1, var.used="by.tree",splitrule="logrank",importance = TRUE)
+
+vs.v <- var.select(object = v.obj, method ="md")
+
+plot(v.obj)
+
+plot.survival(v.obj, subset = mean(1:1222),cens.model = "rfsrc")
 
 
+library(rpart)
+
+diabetes = read.csv(file="base_diabete_ml_sauf_sexe.csv", sep = ';')
+
+rpart(formula, diabetes, weights,
+      model = FALSE, x = FALSE, y = TRUE)
+na.action = na.rpart, method,
 
 
-plot.survival(v.obj, subset = 1:5)
-
-
-v.obj <- rfsrc(Surv(suivi_fin, diabete) ~ ., data = diabetes, 
-               ntree = 500, nodesize = 5)
 
 
 library()
